78aaf20d63ab58d583e5adf0b1ea4981061116e1
Khthonian <anansicorp@tuta.io>
2023-09-14 02:14:50 +0100

    feat(cpp): Complete "Log Levels" exercise

--- cpp/log-levels/log_levels.cpp
+++ cpp/log-levels/log_levels.cpp
@@ -1,5 +1,15 @@
 #include <string>
 
 namespace log_line {
+    std::string message(std::string string) {
+        return string.substr(string.find(" ") + 1);
+    }
 
+    std::string log_level(std::string string) {
+        return string.substr(1, string.find("]") -  1);
+    }
+
+    std::string reformat(std::string string) {
+        return message(string) + " (" + log_level(string) + ")";
+    }
 } // namespace log_line
