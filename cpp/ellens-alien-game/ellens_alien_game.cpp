90b705f6f7db3e734519d52b5257cbe22232ab17
Khthonian <anansicorp@tuta.io>
2023-10-05 14:00:49 +0100

    feat(cpp): Create Alien class

--- cpp/ellens-alien-game/ellens_alien_game.cpp
+++ cpp/ellens-alien-game/ellens_alien_game.cpp
@@ -1,4 +1,19 @@
 namespace targets {
 // TODO: Insert the code for the alien class here
+    class Alien {
+        public:
+            Alien(int x, int y) {
+                x_coordinate = x;
+                y_coordinate = y;
+            }
 
+            int get_health() {
+                return health;
+            }
+
+        private:
+            int x_coordinate;
+            int y_coordinate;
+            int health = 3;
+    }
 }  // namespace targets
