11e4ccdf7096e4550f113acfc9aff6d344900631
Khthonian <anansicorp@tuta.io>
2023-10-08 17:30:37 +0100

    feat(cpp): Complete "Ellen's Alien Game" exercise

--- cpp/ellens-alien-game/ellens_alien_game.cpp
+++ cpp/ellens-alien-game/ellens_alien_game.cpp
@@ -11,9 +11,40 @@ namespace targets {
                 return health;
             }
 
-        private:
+            bool hit() {
+                if (health <= 0) {
+                    return true;
+                }
+                health -= 1;
+                
+                return true;
+            }
+
+            bool is_alive() {
+                if (health <= 0) {
+                    return false;      
+                }
+                return true;
+            }
+
+            bool teleport(int x_new, int y_new) {
+                x_coordinate = x_new;
+                y_coordinate = y_new;
+
+                return true;
+            }
+
+            bool collision_detection(Alien z) {
+                if (x_coordinate == z.x_coordinate && y_coordinate == z.y_coordinate) {
+                    return true;
+                }
+                return false;
+            }
+
             int x_coordinate;
             int y_coordinate;
+
+        private:
             int health = 3;
-    }
+    };
 }  // namespace targets
