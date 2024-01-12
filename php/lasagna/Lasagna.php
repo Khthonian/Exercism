3c64019feca69679801702d452d967d56468850a
Khthonian <anansicorp@tuta.io>
2024-01-12 23:46:40 +0000

    feat(php): Complete "Lucian's Luscious Lasagna" exercise

--- php/lasagna/Lasagna.php
+++ php/lasagna/Lasagna.php
@@ -5,12 +5,27 @@ declare(strict_types=1);
 class Lasagna
 {
     // Please define the 'expectedCookTime()' function
+    function expectedCookTime() {
+        return 40;
+    }
 
     // Please define the 'remainingCookTime($elapsed_minutes)' function
+    function remainingCookTime($elapsed_minutes) {
+        return Lasagna::expectedCookTime() - $elapsed_minutes;
+    }
 
     // Please define the 'totalPreparationTime($layers_to_prep)' function
+    function totalPreparationTime($layers_to_prep) {
+        return 2 * $layers_to_prep;
+    }
 
     // Please define the 'totalElapsedTime($layers_to_prep, $elapsed_minutes)' function
+    function totalElapsedTime($layers_to_prep, $elapsed_minutes) {
+        return Lasagna::totalPreparationTime($layers_to_prep) + $elapsed_minutes;
+    }
 
     // Please define the 'alarm()' function
+    function alarm() {
+        return "Ding!";
+    }
 }
