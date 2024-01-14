471f2645bd2c42737ccadcc19450847893edce55
Khthonian <anansicorp@tuta.io>
2024-01-14 16:41:30 +0000

    feat(go): Complete "Weather Forecast" exercise

--- go/weather-forecast/weather_forecast.go
+++ go/weather-forecast/weather_forecast.go
@@ -1,8 +1,13 @@
+// Package weather creates a string for a location and corresponding condition.
 package weather
 
+// CurrentCondition variable to hold the condition.
 var CurrentCondition string
+
+// CurrentLocation variable to hold the location.
 var CurrentLocation string
 
+// Forecast function to return a string describing the weather.
 func Forecast(city, condition string) string {
 	CurrentLocation, CurrentCondition = city, condition
 	return CurrentLocation + " - current weather condition: " + CurrentCondition
