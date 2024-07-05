86fa1e047be5007fb50732c1dfc2a32a05eb35b3
Khthonian <anansicorp@tuta.io>
2024-07-05 15:13:19 +0100

    feat(csharp): Complete "Lucian's Luscious Lasagna" exercise

--- csharp/lucians-luscious-lasagna/LuciansLusciousLasagna.cs
+++ csharp/lucians-luscious-lasagna/LuciansLusciousLasagna.cs
@@ -1,10 +1,26 @@
 class Lasagna
 {
     // TODO: define the 'ExpectedMinutesInOven()' method
+    public int ExpectedMinutesInOven()
+    {
+        return 40;
+    }
 
     // TODO: define the 'RemainingMinutesInOven()' method
+    public int RemainingMinutesInOven(int mins)
+    {
+        return ExpectedMinutesInOven() - mins;
+    }
 
     // TODO: define the 'PreparationTimeInMinutes()' method
+    public int PreparationTimeInMinutes(int layers)
+    {
+        return 2 * layers;
+    }
 
     // TODO: define the 'ElapsedTimeInMinutes()' method
+    public int ElapsedTimeInMinutes(int layers, int minsInOven)
+    {
+        return PreparationTimeInMinutes(layers) + minsInOven;
+    }
 }
