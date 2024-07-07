d69444a2fd10daced34becb6e826c05f366e68b9
Khthonian <anansicorp@tuta.io>
2024-07-07 11:09:49 +0100

    feat(csharp): Complete "Elon's Toys" exercise

--- csharp/elons-toys/ElonsToys.cs
+++ csharp/elons-toys/ElonsToys.cs
@@ -2,23 +2,29 @@ using System;
 
 class RemoteControlCar
 {
+    private int _battery = 100;
+    private int _distance = 0;
+    
     public static RemoteControlCar Buy()
     {
-        throw new NotImplementedException("Please implement the (static) RemoteControlCar.Buy() method");
+        return new RemoteControlCar();
     }
 
     public string DistanceDisplay()
     {
-        throw new NotImplementedException("Please implement the RemoteControlCar.DistanceDisplay() method");
+        return "Driven " + _distance + " meters";
     }
 
     public string BatteryDisplay()
     {
-        throw new NotImplementedException("Please implement the RemoteControlCar.BatteryDisplay() method");
+        if (_battery <= 0) return "Battery empty";
+        return "Battery at " + _battery + "%";
     }
 
     public void Drive()
     {
-        throw new NotImplementedException("Please implement the RemoteControlCar.Drive() method");
+        if (_battery <= 0) return;
+        _battery -= 1;
+        _distance += 20;
     }
 }
