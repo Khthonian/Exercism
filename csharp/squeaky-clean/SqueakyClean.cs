0246871cf9d7a23911dfc8ec4308b86a860853b4
Khthonian <anansicorp@tuta.io>
2024-07-07 12:55:04 +0100

    feat(csharp): Complete "Squeaky Clean" exercise

--- csharp/squeaky-clean/SqueakyClean.cs
+++ csharp/squeaky-clean/SqueakyClean.cs
@@ -1,9 +1,25 @@
-using System;
-
+using System.Text;
 public static class Identifier
 {
+    private static bool IsGreekLowercase(char c) => c is >= 'α' and <= 'ω';
+    
     public static string Clean(string identifier)
     {
-        throw new NotImplementedException($"Please implement the (static) Identifier.Clean() method");
+        StringBuilder stringBuilder = new StringBuilder();
+        bool afterDash = false;
+        foreach (var c in identifier)
+        {
+            stringBuilder.Append(c switch
+            {
+                _ when char.IsWhiteSpace(c) => '_',
+                _ when char.IsControl(c) => "CTRL",
+                _ when IsGreekLowercase(c) => default,
+                _ when afterDash => char.ToUpperInvariant(c),
+                _ when char.IsLetter(c) => c,
+                _ => default,
+            });
+            afterDash = c.Equals('-');
+        }
+        return stringBuilder.ToString();
     }
 }
