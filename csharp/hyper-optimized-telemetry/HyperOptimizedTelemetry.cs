3d9f86a44139724e575cf7bf32e37c19c5e89f4b
Khthonian <anansicorp@tuta.io>
2024-07-07 15:14:36 +0100

    feat(csharp): Complete "Hyper-optimized Telemetry" exercise

--- csharp/hyper-optimized-telemetry/HyperOptimizedTelemetry.cs
+++ csharp/hyper-optimized-telemetry/HyperOptimizedTelemetry.cs
@@ -1,14 +1,30 @@
 using System;
+using System.Linq;
 
 public static class TelemetryBuffer
 {
     public static byte[] ToBuffer(long reading)
     {
-        throw new NotImplementedException("Please implement the static TelemetryBuffer.ToBuffer() method");
+        var bytes = reading switch
+        {
+            < int.MinValue => BitConverter.GetBytes(reading).Prepend((byte)(256 - 8)),
+            < short.MinValue => BitConverter.GetBytes((int)reading).Prepend((byte)(256 - 4)),
+            < ushort.MinValue => BitConverter.GetBytes((short)reading).Prepend((byte)(256 - 2)),
+            <= ushort.MaxValue => BitConverter.GetBytes((ushort)reading).Prepend((byte)2),
+            <= int.MaxValue => BitConverter.GetBytes((int)reading).Prepend((byte)(256 - 4)),
+            <= uint.MaxValue => BitConverter.GetBytes((uint)reading).Prepend((byte)4),
+            _ => BitConverter.GetBytes(reading).Prepend((byte)(256 - 8)),
+        };
+
+        var enumerable = bytes as byte[] ?? bytes.ToArray();
+        return enumerable.Concat(new byte[9 - enumerable.Length]).ToArray();
     }
 
-    public static long FromBuffer(byte[] buffer)
+    public static long FromBuffer(byte[] buffer) => buffer[0] switch
     {
-        throw new NotImplementedException("Please implement the static TelemetryBuffer.FromBuffer() method");
-    }
+        256 - 8 or 4 or 2 => BitConverter.ToInt64(buffer, 1),
+        256 - 4 => BitConverter.ToInt32(buffer, 1),
+        256 - 2 => BitConverter.ToInt16(buffer, 1),
+        _ => 0,
+    };
 }
