a55037c66fbb9339e677e72d9279114d4526e675
Khthonian <anansicorp@tuta.io>
2024-07-08 14:45:56 +0100

    feat(csharp): Complete "Logs, Logs, Logs!" exercise

--- csharp/logs-logs-logs/LogsLogsLogs.cs
+++ csharp/logs-logs-logs/LogsLogsLogs.cs
@@ -1,16 +1,36 @@
 using System;
 
 // TODO: define the 'LogLevel' enum
+public enum LogLevel
+{
+    Trace = 1,
+    Debug = 2,
+    Info = 4,
+    Warning = 5,
+    Error = 6,
+    Fatal = 42,
+    Unknown = 0,
+}
 
 static class LogLine
 {
     public static LogLevel ParseLogLevel(string logLine)
     {
-        throw new NotImplementedException("Please implement the (static) LogLine.ParseLogLevel() method");
+        string log = logLine[..5];
+        return log switch
+        {
+            "[TRC]" => LogLevel.Trace,
+            "[DBG]" => LogLevel.Debug,
+            "[INF]" => LogLevel.Info,
+            "[WRN]" => LogLevel.Warning,
+            "[ERR]" => LogLevel.Error,
+            "[FTL]" => LogLevel.Fatal,
+            _ => LogLevel.Unknown
+        };
     }
 
     public static string OutputForShortLog(LogLevel logLevel, string message)
     {
-        throw new NotImplementedException("Please implement the (static) LogLine.OutputForShortLog() method");
+        return $"{(int)logLevel}:{message}";
     }
 }
