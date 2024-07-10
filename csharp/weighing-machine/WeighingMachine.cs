f9c2b08dea4fccbfb4f9ec0fe3c727719320e04d
Khthonian <anansicorp@tuta.io>
2024-07-10 09:20:15 +0100

    feat(csharp): Complete "Weighing Machine" exercise

--- csharp/weighing-machine/WeighingMachine.cs
+++ csharp/weighing-machine/WeighingMachine.cs
@@ -1,12 +1,39 @@
 using System;
 
-class WeighingMachine
+class WeighingMachine(int precision)
 {
     // TODO: define the 'Precision' property
+    public int Precision { get; private set; } = precision;
 
     // TODO: define the 'Weight' property
+    private double _weight;
+    public double Weight
+    {
+        get => _weight;
+        set
+        {
+            if (value < 0)
+            {
+                throw new ArgumentOutOfRangeException();
+            }
+
+            _weight = value;
+            DisplayWeight = (Weight - TareAdjustment).ToString($"F{Precision}") + " kg";
+        }
+    }
 
     // TODO: define the 'DisplayWeight' property
+    public string DisplayWeight { get; private set; }
 
     // TODO: define the 'TareAdjustment' property
+    private double _tareAdjustment = 5.0;
+    public double TareAdjustment
+    {
+        get => _tareAdjustment;
+        set
+        {
+            _tareAdjustment = value;
+            DisplayWeight = (Weight - TareAdjustment).ToString($"F{Precision}") + " kg";
+        }
+    }
 }
