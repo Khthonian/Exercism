4481e10c35186f1658a00b28f35637bc77a2f104
Khthonian <anansicorp@tuta.io>
2024-08-15 14:08:15 +0100

    feat(js): Complete "Lasagna Master" exercise

--- javascript/lasagna-master/lasagna-master.js
+++ javascript/lasagna-master/lasagna-master.js
@@ -10,3 +10,49 @@
  *   ...
  * }
  */
+
+export function cookingStatus(remainingTime) {
+  if (remainingTime === 0) {
+    return "Lasagna is done.";
+  } else if (remainingTime > 0) {
+    return "Not done, please wait.";
+  } else {
+    return "You forgot to set the timer.";
+  }
+}
+
+export function preparationTime(layers, time = 2) {
+  let number = layers.length;
+  return number * time;
+}
+
+export function quantities(layers) {
+  let quantities = {
+    noodles: 0,
+    sauce: 0,
+  };
+
+  for (let i = 0; i < layers.length; i++) {
+    if (layers[i] === "noodles") {
+      quantities.noodles += 50;
+    } else if (layers[i] === "sauce") {
+      quantities.sauce += 0.2;
+    }
+  }
+
+  return quantities;
+}
+
+export function addSecretIngredient(friendsList, myList) {
+  myList.push(friendsList[friendsList.length - 1]);
+}
+
+export function scaleRecipe(recipe, portions = 2) {
+  let scaledRecipe = { ...recipe };
+
+  for (let ingredient in scaledRecipe) {
+    scaledRecipe[ingredient] *= portions / 2;
+  }
+
+  return scaledRecipe;
+}
